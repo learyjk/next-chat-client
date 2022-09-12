2e53acc2f44ed318f826ee6d82b94ee814db0bba
Keegan Leary <leary.keegan@gmail.com>
2022-09-11 23:11:28 -0700

    nanoid

--- context/socket.context.tsx
+++ context/socket.context.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { SOCKET_URL } from "../config/default";
+import { nanoid } from "nanoid";
 
 interface Context {
   socket: Socket;
